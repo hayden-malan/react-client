29fb6df763553fadbf053f60eb1e19623703ca25
kahu-2018 <fake@fake.com>
2018-02-21 12:17:40 +1300

    tweaks to index

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
 
-render(<App name='Hayden Github Page' />, document.querySelector('main'))
-console.log('welcome to Hayden Github Page')
+render(<App name="Hayden's Blog" />, document.querySelector('main'))
+console.log("Welcome to Hayden's blog.")
